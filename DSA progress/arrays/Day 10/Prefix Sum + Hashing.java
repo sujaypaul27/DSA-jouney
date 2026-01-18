82e086c6d8115f5ca562751f9c5c6bba7c7e8b15
sujay <sujaypaul2711@gmail.com>
2026-01-18 17:27:58 +0530

    code 1 uploded

---
+++ DSA progress/arrays/Day 10/Prefix Sum + Hashing.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) 
+    {
+        Scanner sc =new Scanner (System.in);
+        
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter target k value : ");
+        int target =sc.nextInt();
+        HashSet<Integer> set =new HashSet<>();
+        int currentsum=0;
+        boolean found =false;
+        
+        for(int i=0;i<n;i++)
+        {
+            currentsum+=arr[i];
+            if(currentsum==target)
+            {
+                found =true;
+                break;
+                
+            }
+            if(set.contains(currentsum-target))
+            {
+                found=true;
+                break;
+            }
+            set.add(currentsum);
+        }
+        if(found)
+        {
+            System.out.println("  Found ");
+        }
+        else
+        {
+            System.out.println(" Not Found ");
+        }
+        for(int a:set){
+            System.out.println(a);
+        }
+        
+    }
