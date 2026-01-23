defce43333c9ca2c011bd70d1647898b830f7aa6
sujay <sujaypaul2711@gmail.com>
2026-01-23 20:51:26 +0530

    Day 14 Code 1 updated

---
+++ DSA progress/arrays/Day 14/to Count no of possibility.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Main{
+    public static void main(String [] args)
+    {
+        int[] arr = {-5, -2, 0, 1, 3, 7};
+        int target = 2;
+        Arrays.sort(arr);
+        int left=0;
+        int right =arr.length-1;
+        
+        int bestleft=0;
+        int bestright=0;
+        int count =0;
+        int closest;
+        while(left<right)
+        {
+            int sum =arr[left]+arr[right];
+            int diff=Math.abs(sum-target);
+            if (diff==0)
+            {
+               
+                bestleft=arr[left];
+                bestright=arr[right];
+                count ++;
+            }
+            if(sum<target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        System.out.println("Best left and right is : "+bestleft+" , "+ bestright);
+        System.out.println("Closest is " + (bestleft+bestright));
+        System.out.println("Count is " + (count));
+    }
+}
