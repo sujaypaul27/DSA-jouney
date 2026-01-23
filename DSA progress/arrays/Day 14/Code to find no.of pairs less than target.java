37116b2283f9bb071dbea6e909ea0e841a28afc3
sujay <sujaypaul2711@gmail.com>
2026-01-23 21:13:47 +0530

    Pushed code 2 fixed version

--- DSA progress/arrays/Day 14/Code to find no.of pairs less than target.java
+++ DSA progress/arrays/Day 14/Code to find no.of pairs less than target.java
@@ -1,5 +1,33 @@
-package DSA progress.arrays.Day 14;
-
-public class Code to find no.of pairs less than target {
-    
+import java.util.*;
+class Main{
+    public static void main(String [] args)
+    {
+        int[] arr = {-5, -2, 0, 1, 3, 7};
+        int target = 2;
+        Arrays.sort(arr);
+        int left=0;
+        int right =arr.length-1;
+        
+        int count =0;
+        
+        while(left<right)
+        {
+            int sum =arr[left]+arr[right];
+            
+            if (sum<target)
+            {
+               
+             
+                count +=(right-left);
+                left++;
+            }
+           
+            else
+            {
+                right--;
+            }
+        }
+      
+        System.out.println("Count is " + count);
+    }
 }
