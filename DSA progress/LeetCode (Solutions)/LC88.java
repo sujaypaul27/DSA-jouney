22a19e9dec4f057449eb445c9c3df58f3cb23c71
sujay <sujaypaul2711@gmail.com>
2026-01-28 20:42:33 +0530

    LC 88 updated

--- DSA progress/LeetCode (Solutions)/LC88.java
+++ DSA progress/LeetCode (Solutions)/LC88.java
@@ -1,5 +1,31 @@
-package LeetCode (Solutions);
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int left=m-1;
+        int right=n-1;
+        int k=m+n-1;
 
-public class LC88 {
-    
+        while(left>=0 &right>=0)
+        {
+            if(nums2[right]>nums1[left])
+            {
+                nums1[k]=nums2[right];
+                right--;
+            }
+            else
+            {
+                nums1[k]=nums1[left];
+                left--;
+
+            }
+
+            k--;
+        }
+        while(right>=0)
+        {
+            nums1[k]=nums2[right];
+            right--;
+            k--;
+        }
+        
+    }
 }
