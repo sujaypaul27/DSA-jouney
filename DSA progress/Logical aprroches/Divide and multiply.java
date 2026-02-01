344fc6c5a5e9e77b0d51800c6c3938fbb5cd030d
sujay <sujaypaul2711@gmail.com>
2026-02-01 22:21:03 +0530

    Updated Code

---
+++ DSA progress/Logical aprroches/Divide and multiply.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int x=8;
+       if(((x/2)*2)==x)
+       {
+           System.out.println("True");
+           
+       }
+       else
+       {
+           System.out.println("False");
+       }
+    }
+}
