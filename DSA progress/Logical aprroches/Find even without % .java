f4e634e4737755fed22ee3de8e2e73883a40457f
sujay <sujaypaul2711@gmail.com>
2026-02-01 22:05:30 +0530

    Code updated

--- DSA progress/Logical aprroches/Find even without % .java
+++ DSA progress/Logical aprroches/Find even without % .java
@@ -1,5 +1,23 @@
-package DSA progress.Logical aprroches;
 
-public class Find even without %  {
-    
+class Main {
+    public static void main(String[] args) {
+        int x=10;
+        while(true)
+        {
+            x=x-2;
+            if(x==0)
+            {
+                System.out.print("Even");
+                break;
+            }
+             if (x==1)
+            {
+                System.out.print("odd");
+                break;
+            }
+           
+            
+        }
+         
+    }
 }
