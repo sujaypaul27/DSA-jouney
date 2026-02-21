e88f5a4c9b8b7e6ae0218a6c45c4274bdeb02e24
Sujay Paul <sujaypaul2711@gmail.com>
2026-02-21 22:44:10 +0530

    Solution updated

--- DSA progress/LeetCode (Solutions)/LC(1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold).java
+++ DSA progress/LeetCode (Solutions)/LC(1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold).java
@@ -1,3 +1,24 @@
-public class LC() {
-    
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int n=arr.length;
+        int sum=0;
+        int count=0;
+        for(int i=0;i<k;i++)
+        {
+            sum+=arr[i];
+        }
+        if(sum/k >=threshold)
+        {
+            count++;
+        }
+        for(int j=k;j<n;j++)
+        {
+            sum+=arr[j]-arr[j-k];
+            if(sum/k >=threshold)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
