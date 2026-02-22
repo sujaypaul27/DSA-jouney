14033a84f7db84f5161c64af571dc82f7d80227b
Sujay Paul <sujaypaul2711@gmail.com>
2026-02-22 23:02:40 +0530

    Code updated

--- DSA progress/LeetCode (Solutions)/1876. Substrings of Size Three with Distinct Characters.java
+++ DSA progress/LeetCode (Solutions)/1876. Substrings of Size Three with Distinct Characters.java
@@ -1,3 +1,16 @@
-public class 1876. Substrings of Size Three with Distinct Characters {
-    
+class Solution {
+    public int countGoodSubstrings(String s) {
+        int count =0;
+        for(int i=0;i<s.length()-2;i++)
+        {
+            char a=s.charAt(i);
+            char b=s.charAt(i+1);
+            char c=s.charAt(i+2);
+            if(a!=b && b!=c && c!=a)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
