7e10a5bbdbf88870d4abd463f4fe1b2645fd7995
Sujay Paul <sujaypaul2711@gmail.com>
2026-02-25 23:05:30 +0530

    Solution updated

--- DSA progress/LeetCode (Solutions)/904. Fruit Into Baskets.java
+++ DSA progress/LeetCode (Solutions)/904. Fruit Into Baskets.java
@@ -1,1 +1,32 @@
-fsfa
+class Solution {
+    public int totalFruit(int[] fruits) {
+        if(fruits.length==0  ){//to avoid edge case
+            return 0;
+        }
+        {
+
+        }
+
+        int left=0;
+        int max=0;
+        HashMap<Integer,Integer> count=new HashMap<>();
+
+        for(int right=0;right<fruits.length;right++)
+        {
+            count.put(fruits[right],count.getOrDefault(fruits[right],0)+1);
+        
+        while(count.size()>2)
+        {
+            count.put(fruits[left],count.get(fruits[left])-1);
+            if(count.get(fruits[left])==0)
+            {
+                count.remove(fruits[left]);
+            }
+            left++;
+           
+        }
+         max=Math.max(max,right-left+1);
+        }
+        return max;
+    }
+}
