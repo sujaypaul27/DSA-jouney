28877b2cafd959613af39f460a0ed1403515301b
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-01 23:11:19 +0530

    Explanation updated

--- DSA progress/LeetCode (Solutions)/877. Stone Game.java
+++ DSA progress/LeetCode (Solutions)/877. Stone Game.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
        
-        return true;
+        return true;// Alice can always win by following a simple strategy: she can always choose the pile with the most stones. Since there are an even number of piles, Alice can always ensure that she gets the larger half of the piles, guaranteeing her victory.
     }
 }
