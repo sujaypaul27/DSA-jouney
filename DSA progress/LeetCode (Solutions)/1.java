f613248c6db8c09bf8cc5ea8e088a49ef3829cf6
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-02 23:05:45 +0530

    New code

--- DSA progress/LeetCode (Solutions)/1.java
+++ DSA progress/LeetCode (Solutions)/1.java
@@ -1,3 +1,23 @@
-public class 1 {
-    
+class Solution {
+
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        return count(nums, right) - count(nums, left - 1);
+    }
+
+    // Count subarrays where max <= k
+    private int count(int[] nums, int k) {
+        int result = 0;
+        int length = 0;
+
+        for (int num : nums) {
+            if (num <= k) {
+                length++;           // extend window
+            } else {
+                length = 0;         // break window
+            }
+            result += length;
+        }
+
+        return result;
+    }
 }
