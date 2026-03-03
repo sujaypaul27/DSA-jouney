37b1d6a638afd436aff11ba2b1cf600e52c17bb5
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-03 23:07:02 +0530

    NEw solution

--- DSA progress/LeetCode (Solutions)/LC(165. Compare Version Numbers).java
+++ DSA progress/LeetCode (Solutions)/LC(165. Compare Version Numbers).java
@@ -1,1 +1,30 @@
-adsad
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int i = 0, j = 0;
+        int n1 = version1.length();
+        int n2 = version2.length();
+        
+        while (i < n1 || j < n2) {
+            
+            int num1 = 0;
+            while (i < n1 && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+            
+            int num2 = 0;
+            while (j < n2 && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+            
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+            
+            i++; // skip '.'
+            j++; // skip '.'
+        }
+        
+        return 0;
+    }
+}
