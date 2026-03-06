9fc0059055c430390ecb1df6f73be9ce0879cedf
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-06 19:31:22 +0530

    SOlution added

--- DSA progress/LeetCode (Solutions)/LC(141. Linked List Cycle).java
+++ DSA progress/LeetCode (Solutions)/LC(141. Linked List Cycle).java
@@ -1,3 +1,31 @@
-public class LC(141. Linked List Cycle) {
-    
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if(head == null || head.next == null) return false;
+
+        ListNode slow = head;// slow pointer
+        ListNode fast = head;// fast pointer
+
+        while(fast != null && fast.next != null){// while fast pointer and its next are not null
+            slow = slow.next;          // move 1 step
+            fast = fast.next.next;     // move 2 steps
+
+            if(slow == fast){// pointers meet
+                return true;           // cycle detected
+            }
+        }
+
+        return false;                  // no cycle
+    }
 }
