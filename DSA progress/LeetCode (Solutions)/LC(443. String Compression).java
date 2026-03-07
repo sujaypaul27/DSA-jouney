afe82b373a4c247a297d78cd7e23b5fea7b59453
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-07 23:31:50 +0530

    New code

--- DSA progress/LeetCode (Solutions)/LC(443. String Compression).java
+++ DSA progress/LeetCode (Solutions)/LC(443. String Compression).java
@@ -1,1 +1,30 @@
-sdad
+sdadclass Solution {
+    public int compress(char[] chars) {
+        int index = 0;   // position to write
+        int i = 0;// position to read
+
+        while (i < chars.length) {
+            char current = chars[i];
+            int count = 0;
+
+            // count same characters
+            while (i < chars.length && chars[i] == current) {
+                i++;
+                count++;
+            }
+
+            // write character
+            chars[index++] = current;
+
+            // write count if greater than 1
+            if (count > 1) {
+                String num = Integer.toString(count);
+                for (char c : num.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+}
