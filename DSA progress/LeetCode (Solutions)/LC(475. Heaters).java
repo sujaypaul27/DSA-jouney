e55b1e8064d8d9bc5a25dd3f4a2c1d846d74e04a
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-11 23:23:01 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(475. Heaters).java
+++ DSA progress/LeetCode (Solutions)/LC(475. Heaters).java
@@ -1,1 +1,23 @@
-dsadsada
+
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        int radius = 0;
+
+        for (int house : houses) {
+            int index = Arrays.binarySearch(heaters, house);
+
+            if (index < 0) {
+                index = -index - 1;
+            }
+
+            int dist1 = index < heaters.length ? Math.abs(heaters[index] - house) : Integer.MAX_VALUE;
+            int dist2 = index > 0 ? Math.abs(house - heaters[index - 1]) : Integer.MAX_VALUE;
+
+            int closest = Math.min(dist1, dist2);
+            radius = Math.max(radius, closest);
+        }
+
+        return radius;
+    }
+}
