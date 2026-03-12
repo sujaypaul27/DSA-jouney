9b54b7038a2a6c28ae96de8476ce47a368f8b7ec
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-12 22:44:38 +0530

    Code added

--- DSA progress/LeetCode (Solutions)/LC(1089. Duplicate Zeros).java
+++ DSA progress/LeetCode (Solutions)/LC(1089. Duplicate Zeros).java
@@ -1,1 +1,31 @@
-asdadsad
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        int zeros = 0;
+
+        // Count zeros
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) zeros++;
+        }
+
+        int i = n - 1;
+        int j = n + zeros - 1;
+
+        // Traverse from the end
+        while (i < j) {
+            if (j < n) {
+                arr[j] = arr[i];
+            }
+
+            if (arr[i] == 0) {
+                j--;
+                if (j < n) {
+                    arr[j] = 0;
+                }
+            }
+
+            i--;
+            j--;
+        }
+    }
+}
