4990ba5915f656f7f02850b2e848dfdb412ed139
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-15 23:13:51 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(457. Circular Array Loop).java
+++ DSA progress/LeetCode (Solutions)/LC(457. Circular Array Loop).java
@@ -1,1 +1,46 @@
-sda
+class Solution {
+    
+    public boolean circularArrayLoop(int[] nums) {
+        int n = nums.length;
+        
+        for(int i = 0; i < n; i++){
+            
+            int slow = i;
+            int fast = i;
+            
+            boolean direction = nums[i] > 0;
+            
+            while(true){
+                
+                slow = next(nums, direction, slow);
+                if(slow == -1) break;
+                
+                fast = next(nums, direction, fast);
+                if(fast == -1) break;
+                
+                fast = next(nums, direction, fast);
+                if(fast == -1) break;
+                
+                if(slow == fast) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private int next(int[] nums, boolean direction, int index){
+        
+        boolean currentDirection = nums[index] > 0;
+        
+        if(direction != currentDirection)
+            return -1;
+        
+        int n = nums.length;
+        int nextIndex = ((index + nums[index]) % n + n) % n;
+        
+        if(nextIndex == index)
+            return -1;
+        
+        return nextIndex;
+    }
+}
