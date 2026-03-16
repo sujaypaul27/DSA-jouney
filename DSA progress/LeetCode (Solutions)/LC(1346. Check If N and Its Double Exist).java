57127e5efe5f7e5dedf81d8d15ce929abde4ca1a
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-16 23:51:06 +0530

    Comments addded

--- DSA progress/LeetCode (Solutions)/LC(1346. Check If N and Its Double Exist).java
+++ DSA progress/LeetCode (Solutions)/LC(1346. Check If N and Its Double Exist).java
@@ -1,18 +1,18 @@
 import java.util.HashSet;
 
 class Solution {
-    public boolean checkIfExist(int[] arr) {
-        HashSet<Integer> set = new HashSet<>();
+    public boolean checkIfExist(int[] arr) {// TC: O(n), SC: O(n)
+        HashSet<Integer> set = new HashSet<>();// to store the elements of the array
         
-        for (int num : arr) {
+        for (int num : arr) {// iterate through the array
             
-            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
-                return true;
+            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {// if the set contains either num*2 or num/2 (only if num is even),
+                return true;// if the set contains either num*2 or num/2 (only if num is even), return true
             }
             
-            set.add(num);
+            set.add(num);// add the current number to the set
         }
         
-        return false;
+        return false;// if no such pair is found, return false
     }
 }
