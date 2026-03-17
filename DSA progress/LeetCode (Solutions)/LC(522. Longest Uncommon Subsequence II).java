e2542e8f00f876a853127e28d475b1029d217f8d
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-17 23:26:17 +0530

    New code

--- DSA progress/LeetCode (Solutions)/LC(522. Longest Uncommon Subsequence II).java
+++ DSA progress/LeetCode (Solutions)/LC(522. Longest Uncommon Subsequence II).java
@@ -1,1 +1,41 @@
-dsd
+import java.util.*;
+
+class Solution {
+    public int findLUSlength(String[] strs) {
+        // Sort by descending length
+        Arrays.sort(strs, (a, b) -> b.length() - a.length());
+        
+        for (int i = 0; i < strs.length; i++) {
+            boolean isUncommon = true;
+            
+            for (int j = 0; j < strs.length; j++) {
+                if (i == j) continue;
+                
+                // If strs[i] is subsequence of strs[j]
+                if (isSubsequence(strs[i], strs[j])) {
+                    isUncommon = false;
+                    break;
+                }
+            }
+            
+            if (isUncommon) {
+                return strs[i].length();
+            }
+        }
+        
+        return -1;
+    }
+    
+    private boolean isSubsequence(String a, String b) {
+        int i = 0, j = 0;
+        
+        while (i < a.length() && j < b.length()) {
+            if (a.charAt(i) == b.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        
+        return i == a.length();
+    }
+}
