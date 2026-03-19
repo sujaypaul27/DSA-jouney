7e806a4651823ae69c71e70a41efcfb67f74b295
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-19 23:24:54 +0530

    Solution

--- DSA progress/LeetCode (Solutions)/LC(844. Backspace String Compare).java
+++ DSA progress/LeetCode (Solutions)/LC(844. Backspace String Compare).java
@@ -1,3 +1,54 @@
 public class LC(844. Backspace String Compare) {
     
 }
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        int skipS = 0;
+        int skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+
+            // Process string s
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            // Process string t
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            // Compare characters
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) != t.charAt(j)) return false;
+            } else {
+                // If one string finished but other didn't
+                if (i >= 0 || j >= 0) return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+}
