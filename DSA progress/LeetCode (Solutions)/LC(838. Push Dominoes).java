2f1918733bf22dee7fed8fb25cdae70c93a24444
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-23 23:47:30 +0530

    Solution addedf

--- DSA progress/LeetCode (Solutions)/LC(838. Push Dominoes).java
+++ DSA progress/LeetCode (Solutions)/LC(838. Push Dominoes).java
@@ -1,1 +1,37 @@
-sdad
+class Solution {
+    public String pushDominoes(String dominoes) {
+        StringBuilder s = new StringBuilder();
+        s.append('L').append(dominoes).append('R');
+        
+        char[] arr = s.toString().toCharArray();
+        int i = 0;
+        
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] == '.') continue;
+            
+            int middle = j - i - 1;
+            
+            if (arr[i] == arr[j]) {
+                for (int k = 1; k <= middle; k++) {
+                    s.setCharAt(i + k, arr[i]);
+                }
+            } 
+            else if (arr[i] == 'R' && arr[j] == 'L') {
+                for (int k = 1; k <= middle; k++) {
+                    if (k <= middle / 2) {
+                        s.setCharAt(i + k, 'R');
+                    } else if (k == middle / 2 + 1 && middle % 2 == 1) {
+                        s.setCharAt(i + k, '.');
+                    } else {
+                        s.setCharAt(i + k, 'L');
+                    }
+                }
+            }
+            // L...R → do nothing
+            
+            i = j;
+        }
+        
+        return s.substring(1, s.length() - 1);
+    }
+}
