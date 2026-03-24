fbd6d85230ae181d774d30d33c628641e2dd460f
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-24 23:23:37 +0530

    New code

--- DSA progress/LeetCode (Solutions)/LC().java
+++ DSA progress/LeetCode (Solutions)/LC().java
@@ -1,1 +1,21 @@
-dsad
+import java.util.*;
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++; // pair light + heavy
+            }
+            right--; // heavy always goes
+            boats++;
+        }
+        
+        return boats;
+    }
+}
