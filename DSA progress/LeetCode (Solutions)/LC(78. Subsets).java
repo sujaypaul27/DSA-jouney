c6b92d00ebf47eb79b3678e5f3c148059341690f
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-27 23:14:33 +0530

    New code

--- DSA progress/LeetCode (Solutions)/LC(78. Subsets).java
+++ DSA progress/LeetCode (Solutions)/LC(78. Subsets).java
@@ -1,3 +1,25 @@
-public class LC(78. Subsets) {
-    
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        // Add current subset
+        result.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+            // Choose
+            current.add(nums[1]);
+
+            // Explore
+            backtrack(i + 1, nums, current, result);
+
+            // Undo (backtrack)
+            current.remove(current.size() - 1);
+        }
+    }
 }
