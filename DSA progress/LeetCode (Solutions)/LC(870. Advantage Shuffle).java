83bf2136fb8e7724277ae4d36a31e96a8d105033
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-28 22:26:36 +0530

    comments added

--- DSA progress/LeetCode (Solutions)/LC(870. Advantage Shuffle).java
+++ DSA progress/LeetCode (Solutions)/LC(870. Advantage Shuffle).java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] advantageCount(int[] A, int[] B) {
-        int n = A.length;
+        int n = A.length;//
         Arrays.sort(A);
 
         int[][] b = new int[n][2];
@@ -9,21 +9,21 @@ class Solution {
             b[i][1] = i;
         }
 
-        Arrays.sort(b, (x, y) -> x[0] - y[0]);
+        Arrays.sort(b, (x, y) -> x[0] - y[0]);// Arrays.sort(b, (x, y) -> x[0] - y[0]);
 
         int low = 0, high = n - 1;
         int[] res = new int[n];
 
         for (int i = n - 1; i >= 0; i--) {
             if (A[high] > b[i][0]) {
-                res[b[i][1]] = A[high];
+                res[b[i][1]] = A[high];//
                 high--;
-            } else {
+            } else {// 
                 res[b[i][1]] = A[low];
-                low++;
+                low++;// low++;
             }
         }
 
-        return res;
+        return res;// return res;
     }
 }
