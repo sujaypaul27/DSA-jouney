c24fd335b5ca7e44b243464a5e507961281ac104
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-29 23:36:33 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(485. Max Consecutive Ones).java
+++ DSA progress/LeetCode (Solutions)/LC(485. Max Consecutive Ones).java
@@ -1,1 +1,18 @@
-dsa
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int currentcount=0;
+        int maxcount=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1)
+            {
+                currentcount++;
+                
+            }else{
+                maxcount=Math.max(maxcount,currentcount);
+                currentcount=0;
+            }
+        }
+        maxcount=Math.max(maxcount,currentcount);
+        return maxcount;
+    }
+}
