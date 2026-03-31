c4da6c26fb9e981deaf7a6fa546a379fef7930ed
Sujay Paul <sujaypaul2711@gmail.com>
2026-03-31 22:52:52 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(1004. Max Consecutive Ones III).java
+++ DSA progress/LeetCode (Solutions)/LC(1004. Max Consecutive Ones III).java
@@ -1,1 +1,22 @@
-sda
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n=nums.length;
+        int left=0;
+        int max=0;
+        int zerocount=0;
+        for(int right=0;right<n;right++){
+            if(nums[right]==0)
+            {
+                zerocount++;
+            }
+            if(zerocount>k){
+                if(nums[left]==0){
+                    zerocount--;
+                }
+                left++;
+            }
+            max=right-left+1;//since right pointer is in last element and didnt end the loop
+        }
+        return max;
+    }
+}
