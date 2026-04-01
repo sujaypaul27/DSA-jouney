6f3444fd7754b13e085c7420092529296a29a2c3
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-01 19:02:03 +0530

    Solution addedf

--- DSA progress/LeetCode (Solutions)/LC(2938. Separate Black and White Balls).java
+++ DSA progress/LeetCode (Solutions)/LC(2938. Separate Black and White Balls).java
@@ -1,4 +1,16 @@
-public class LC(2938. Separate Black and White Balls) {
-    
+class Solution {
+    public long minimumSteps(String s) {
+        long swaps = 0;
+        long countOnes = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '1') {
+                countOnes++;
+            } else {
+                swaps += countOnes;
+            }
+        }
+
+        return swaps;
+    }
 }
-sdada
