92c94736c95ae4a087f76d398bb09728f897deb0
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-04 21:34:05 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(992. Subarrays with K Different Integers2).java
+++ DSA progress/LeetCode (Solutions)/LC(992. Subarrays with K Different Integers2).java
@@ -1,1 +1,24 @@
-dsa
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return calculate(nums,k)-calculate(nums,k-1);
+    }
+    private int calculate(int[] nums,int k){
+        int l=0;
+        int count=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int r=0;r<nums.length;r++)
+        {
+            map.put(nums[r],map.getOrDefault(nums[r],0)+1);
+            while(map.size()>k){
+                map.put(nums[l],map.get(nums[l])-1);
+                if(map.get(nums[l])==0)
+                {
+                    map.remove(nums[l]);
+                }
+                l++;
+            }
+            count+=r-l+1;
+        }
+        return count;
+    }
+}
