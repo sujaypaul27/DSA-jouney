c588d0f39ed00e4a64eab8f1ee7d3e3a3482f946
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-05 23:41:05 +0530

    Uploaded code

--- DSA progress/LeetCode (Solutions)/LC(986. Interval List Intersections).java
+++ DSA progress/LeetCode (Solutions)/LC(986. Interval List Intersections).java
@@ -1,1 +1,25 @@
-asda
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        int i = 0, j = 0;
+        List<int[]> result = new ArrayList<>();
+
+        while (i < firstList.length && j < secondList.length) {
+            int start = Math.max(firstList[i][0], secondList[j][0]);
+            int end = Math.min(firstList[i][1], secondList[j][1]);
+
+            // Check if valid intersection
+            if (start <= end) {
+                result.add(new int[]{start, end});
+            }
+
+            // Move the pointer which ends first
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
