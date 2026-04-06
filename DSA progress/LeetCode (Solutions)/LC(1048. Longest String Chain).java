2f1a7f53cce374503d5a874537d412c6fc09246e
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-06 22:30:29 +0530

    New code added

--- DSA progress/LeetCode (Solutions)/LC(1048. Longest String Chain).java
+++ DSA progress/LeetCode (Solutions)/LC(1048. Longest String Chain).java
@@ -1,1 +1,26 @@
-ds
+class Solution {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        HashMap<String, Integer> dp = new HashMap<>();
+        int maxLen = 1;
+
+        for (String word : words) {
+            int best = 1;
+
+            // Try removing each character
+            for (int i = 0; i < word.length(); i++) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+
+                if (dp.containsKey(prev)) {
+                    best = Math.max(best, dp.get(prev) + 1);
+                }
+            }
+
+            dp.put(word, best);
+            maxLen = Math.max(maxLen, best);
+        }
+
+        return maxLen;
+    }
+}
