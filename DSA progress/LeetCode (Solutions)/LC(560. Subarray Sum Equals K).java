96caade57ada22a826cdd365445b7fb2c9bac611
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-07 01:47:48 +0530

    New code

--- DSA progress/LeetCode (Solutions)/LC(560. Subarray Sum Equals K).java
+++ DSA progress/LeetCode (Solutions)/LC(560. Subarray Sum Equals K).java
@@ -1,3 +1,18 @@
-public class LC(560. Subarray Sum Equals K) {
-    
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count=0;
+        int prefixsum=0;
+        HashMap<Integer,Integer> map=new HashMap<>();//to store values
+        map.put(0,1);//to avoid single k valid element edge case
+        for(int r=0;r<nums.length;r++)
+        {
+            prefixsum+=nums[r];
+            if(map.containsKey(prefixsum-k))
+            {
+                count+=map.get(prefixsum-k);
+            }
+            map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
+        }
+        return count;
+    }
 }
