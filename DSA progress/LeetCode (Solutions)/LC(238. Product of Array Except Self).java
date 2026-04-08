3130acce5008843eda795e229e4e6056118ca3cc
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-09 00:19:53 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(238. Product of Array Except Self).java
+++ DSA progress/LeetCode (Solutions)/LC(238. Product of Array Except Self).java
@@ -1,1 +1,18 @@
-238. Product of Array Except Self
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int[] res=new int[n];
+        res[0]=1;//leftmost will be 1 first
+        for(int l=1;l<n;l++)
+        {
+            res[l]=res[l-1]*nums[l-1];
+        }
+        int rp=1;
+        for(int r=n-1;r>=0;r--)
+        {
+            res[r]=res[r]*rp;
+            rp*=nums[r];
+        }
+        return res;
+    }
+}
