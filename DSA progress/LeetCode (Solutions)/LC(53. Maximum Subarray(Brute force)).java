10a82863a9634a91c29abd09bc77bb4c97afbb03
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-12 01:00:15 +0530

    New code

--- DSA progress/LeetCode (Solutions)/LC(53. Maximum Subarray(Brute force)).java
+++ DSA progress/LeetCode (Solutions)/LC(53. Maximum Subarray(Brute force)).java
@@ -1,1 +1,17 @@
-sda
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ans=nums[0];
+        int n=nums.length;
+        for(int left=0;left<n;left++)
+        {
+            int sum=0;
+            for(int right=left;right<n;right++)
+            {
+                sum+=nums[right];
+                ans=Math.max(ans,sum);//need to calculate max for every single sub array
+            }
+            
+        }
+        return ans;
+    }
+}
