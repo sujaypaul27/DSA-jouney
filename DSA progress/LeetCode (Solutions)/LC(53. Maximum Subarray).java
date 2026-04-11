ff6bedc6cc3c68a480805964ae748465216870d1
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-12 01:19:57 +0530

    OPtimised code updated

--- DSA progress/LeetCode (Solutions)/LC(53. Maximum Subarray).java
+++ DSA progress/LeetCode (Solutions)/LC(53. Maximum Subarray).java
@@ -1,3 +1,13 @@
 public class LC(53. Maximum Subarray) {
-    
+    int n=nums.length;
+        int maxSum=nums[0];
+        int currentSum=0;
+        for(int i=0;i<n;i++)
+        {
+            currentSum+=nums[i];
+            maxSum=Math.max(maxSum,currentSum);
+            if(currentSum<0)
+                currentSum=0;//if sum is negative then we can ignore that part and start from next element
+        }
+        return maxSum;
 }
