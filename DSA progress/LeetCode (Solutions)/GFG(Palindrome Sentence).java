59a0589b9120703bb5d9f38ab493b003609aad4c
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-14 23:34:03 +0530

    Solution added for GFG

--- DSA progress/LeetCode (Solutions)/GFG(Palindrome Sentence).java
+++ DSA progress/LeetCode (Solutions)/GFG(Palindrome Sentence).java
@@ -1,1 +1,30 @@
-sd
+class Solution {
+    public boolean isPalinSent(String s) {
+        // code here
+        int n=s.length();
+        int left=0;
+        int right=n-1;
+        while(left<right)
+        {
+            char l=s.charAt(left);
+            char r=s.charAt(right);
+            if(!Character.isLetterOrDigit(l))
+            {
+                left++;
+            }else if(!Character.isLetterOrDigit(r))
+            {
+                right--;
+            }else
+            {
+                if(Character.toLowerCase(l)!=Character.toLowerCase(r))
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            
+        }
+        return true;
+    }
+}
