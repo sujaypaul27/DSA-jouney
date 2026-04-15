006a07599538034da241bc287f64f20ad5299ffb
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-16 00:35:02 +0530

    Answer added for LC(5)

--- DSA progress/LeetCode (Solutions)/LC(5. Longest Palindromic Substring).java
+++ DSA progress/LeetCode (Solutions)/LC(5. Longest Palindromic Substring).java
@@ -1,3 +1,27 @@
-public class LC(5. Longest Palindromic Substring) {
-    
+class Solution {
+    private int palindromeLength(String s,int left,int right)
+    {
+        while(left>=0&& right<s.length()&&s.charAt(left)==s.charAt(right))
+        {
+            
+            left--;
+            right++;
+        }
+        return right-left-1;//to rget the length in expand arround center pattern since we we shrink left and expand right from given i value
+    }
+    public String longestPalindrome(String s) {
+        int start =0;
+        int end=0;
+        for(int i=0;i<s.length();i++){
+        int oddlength=palindromeLength(s,i,i);//in odd term common will be 1
+        int evenlength=palindromeLength(s,i,i+1);//in even wew have 2 terms
+        int len=Math.max(oddlength,evenlength);//any 1 input will be given so only max value ip is given
+        if(len>(end-start))
+        {
+            start=i-(len-1)/2;
+            end=i+len/2;
+        }
+        }
+        return s.substring(start,end+1);//since it takes from i to i-1 +1 is added
+    }
 }
