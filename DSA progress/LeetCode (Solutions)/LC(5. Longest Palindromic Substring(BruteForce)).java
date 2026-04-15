ab1a893c1df96c9e9d513676b8e475e1c9625f9c
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-15 23:07:35 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(5. Longest Palindromic Substring(BruteForce)).java
+++ DSA progress/LeetCode (Solutions)/LC(5. Longest Palindromic Substring(BruteForce)).java
@@ -1,1 +1,33 @@
-fs
+class Solution {
+    private Boolean isPalindrome(String s,int left,int right)
+    {
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public String longestPalindrome(String s) {
+        String res="";
+        int n=s.length();
+        for(int left=0;left<n;left++)
+        {
+            for(int right=left;right<n;right++)
+            {
+                if(isPalindrome(s,left,right)==true)
+                {
+                    if(right-left+1 > res.length())
+                    {
+                        res=s.substring(left,right+1);//coz it will take from 0 to j-1 elements only
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
