9cbb59e789b1600dbee4ca2c0064fde0e69aa97f
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-17 03:09:38 +0530

    solution for LC 69

--- DSA progress/LeetCode (Solutions)/LC(69. Sqrt(x)).java
+++ DSA progress/LeetCode (Solutions)/LC(69. Sqrt(x)).java
@@ -1,1 +1,30 @@
-sd
+class Solution {
+    public int mySqrt(int x) {
+        //method 1
+        //double ans =Math.sqrt(x);
+        //return (int)ans;
+        if (x < 2) return x;
+        
+        long left = 1;
+        long right = x/2;//long coz constarint says it will be more in number
+        
+        while(left <= right)
+        {
+            long mid = left + ( right - left) / 2;
+            long sq=mid*mid;
+            if ( sq == x )
+            {
+                return (int)mid;
+                
+            }else if( sq > x )
+            {
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+            
+        }
+        return (int)right;
+        
+    }
+}
