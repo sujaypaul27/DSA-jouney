f4d186e93bd70159ec035b89eff4e528363d7ba5
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-20 23:37:57 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(33. Search in Rotated Sorted Array).java
+++ DSA progress/LeetCode (Solutions)/LC(33. Search in Rotated Sorted Array).java
@@ -1,1 +1,31 @@
-dsd
+class Solution {
+    public int search(int[] nums, int target) {
+     int n=nums.length;
+     int left=0;
+     int right=n-1;
+     while(left<=right)
+     {
+        int mid = left+(right-left)/2;
+        if(nums[mid]== target) return mid;
+        else if (nums[left]<=nums[mid])//left part sorted
+        {
+            if(nums[left]<=target && nums[mid]>target)
+            {
+                right=mid-1;
+            }else
+            {
+                left=mid+1;
+            }
+        }else//rightpartsorted
+        {
+            if(nums[mid] < target && target <= nums[right])
+            {
+                left=mid+1;
+            }else{
+                right =mid-1;
+            }
+        }
+     }
+     return -1;
+    }
+}
