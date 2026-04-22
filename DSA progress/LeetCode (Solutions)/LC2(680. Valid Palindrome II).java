966ad4e3817806862a39662689243d03499cc987
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-22 22:53:43 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC2(680. Valid Palindrome II).java
+++ DSA progress/LeetCode (Solutions)/LC2(680. Valid Palindrome II).java
@@ -1,3 +1,30 @@
-public class LC2() {
-    
+class Solution {
+    private boolean isPalindrome(String s,int left,int right){
+        
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int n=s.length();
+        int left=-0;
+        int right=n-1;
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                return isPalindrome(s,left+1,right)||isPalindrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
