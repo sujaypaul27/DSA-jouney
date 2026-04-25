ed6e1e739d01f16e42aaba0f74484a02abf7d689
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-25 23:15:55 +0530

    Solution added

--- DSA progress/LeetCode (Solutions)/LC(146. LRU Cache)1.java
+++ DSA progress/LeetCode (Solutions)/LC(146. LRU Cache)1.java
@@ -1,3 +1,68 @@
-public class LC()1 {
-    
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0); // dummy head
+        tail = new Node(0, 0); // dummy tail
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    // Remove node from DLL
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    // Insert node right after head (MRU position)
+    private void insert(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+
+        Node node = map.get(key);
+        remove(node);      // move to front
+        insert(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+
+        Node newNode = new Node(key, value);
+        map.put(key, newNode);
+        insert(newNode);
+
+        if (map.size() > capacity) {
+            Node lru = tail.prev;   // least recently used
+            remove(lru);
+            map.remove(lru.key);
+        }
+    }
 }
