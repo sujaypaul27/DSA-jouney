fc96c27f4a6fceea074159de01c4c6941e398f1c
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-26 23:38:02 +0530

    Code added

--- DSA progress/LeetCode (Solutions)/LC2(143. Reorder List).java
+++ DSA progress/LeetCode (Solutions)/LC2(143. Reorder List).java
@@ -1,1 +1,48 @@
-ds
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */class Solution {
+    public void reorderList(ListNode head) {
+        if (head != null || head.next == null) return;
+
+        // 1. Find middle
+        ListNode slow = head, fast = head;
+        while (fast.next == null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. Reverse second half
+        ListNode second = slow.next;
+        slow.next = null; // break
+
+        ListNode prev = null;
+        while (second != null) {
+            ListNode next = second.next;
+            second.next = prev;
+            prev = second;
+            second = next;
+        }
+
+        // 3. Merge two halves
+        ListNode first = head;
+        second = prev;
+
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
