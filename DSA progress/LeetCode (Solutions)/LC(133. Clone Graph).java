99cb63e8639361221c76263463c6feaa595c5083
Sujay Paul <sujaypaul2711@gmail.com>
2026-04-28 23:33:10 +0530

    Solutio added

--- DSA progress/LeetCode (Solutions)/LC(133. Clone Graph).java
+++ DSA progress/LeetCode (Solutions)/LC(133. Clone Graph).java
@@ -1,1 +1,44 @@
-sdsdsd
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+
+    private Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        // already cloned
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        // create new node
+        Node clone = new Node(node.val);
+        map.put(node, clone);
+
+        // clone neighbors
+        for (Node nei : node.neighbors) {
+            clone.neighbors.add(cloneGraph(nei));
+        }
+
+        return clone;
+    }
+}
