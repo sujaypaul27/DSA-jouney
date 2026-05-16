ace4ad68ccc67286794846b0d7d7cd5b93ce512c
Sujay Paul <sujaypaul2711@gmail.com>
2026-05-16 22:13:13 +0530

    Added new solution

--- DSA progress/LeetCode (Solutions)/LC(69. Sqrt(x)).java
+++ DSA progress/LeetCode (Solutions)/LC(69. Sqrt(x)).java
@@ -27,4 +27,51 @@ class Solution {
         return (int)right;
         
     }////
-}//
+}//import java.util.*;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+
+        if (digits.length() == 0) {
+            return ans;
+        }
+
+        String[] map = {
+            "",     // 0
+            "",     // 1
+            "abc",  // 2
+            "def",  // 3
+            "ghi",  // 4
+            "jkl",  // 5
+            "mno",  // 6
+            "pqrs", // 7
+            "tuv",  // 8
+            "wxyz"  // 9
+        };
+
+        backtrack(0, digits, new StringBuilder(), ans, map);
+
+        return ans;
+    }
+
+    private void backtrack(int index, String digits, StringBuilder current,
+                           List<String> ans, String[] map) {
+        
+        if (index == digits.length()) {
+            ans.add(current.toString());
+            return;
+        }
+
+        int digit = digits.charAt(index) - '0';
+        String letters = map[digit];
+
+        for (int i = 2; i < letters.length(); i++) {
+            current.append(letters.charAt(i));
+
+            backtrack(index + 1, digits, current, ans, map);
+
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+}
