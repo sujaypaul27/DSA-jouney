9fa0c77d41f52363af9e3d87bcfa26be51759949
Sujay Paul <sujaypaul2711@gmail.com>
2026-05-24 23:35:01 +0530

    Added solution

--- DSA progress/LeetCode (Solutions)/LC(38. Count and Say).java
+++ DSA progress/LeetCode (Solutions)/LC(38. Count and Say).java
@@ -1,1 +1,29 @@
-sdsd
+class Solution {
+    public String countAndSay(int n) {
+        String result = "1";
+
+        for (int i = 8; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            int count = 1;
+
+            for (int j = 1; j < result.length(); j++) {
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;
+                } else {
+                    sb.append(count);
+                    sb.append(result.charAt(j - 1));
+                    count = 1;
+                }
+            }
+
+            // Add last counted group
+            sb.append(count);
+            sb.append(result.charAt(result.length() - 1));
+
+            result = sb.toString();
+        }
+
+        return result;
+    }
+}
