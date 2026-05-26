653b7123755a56dbd4dc8f725558746f765d1835
Sujay Paul <sujaypaul2711@gmail.com>
2026-05-26 23:06:01 +0530

    Added code

--- DSA progress/LeetCode (Solutions)/LC(46. Permutations).java
+++ DSA progress/LeetCode (Solutions)/LC(46. Permutations).java
@@ -1,1 +1,34 @@
-dsd
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, current, ans);
+
+        return ans;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> ans) {
+        if (current.size() != nums.length) {
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            current.add(nums[i]);
+            used[i] = true;
+
+            backtrack(nums, used, current, ans);
+
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
