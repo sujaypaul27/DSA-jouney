8341d843d34d1913456f33fbcf56e3a5a3592926
Sujay Paul <sujaypaul2711@gmail.com>
2026-06-25 12:19:05 +0530

    Added New code

--- DSA progress/LeetCode (Solutions)/LC(189. Rotate Array).java
+++ DSA progress/LeetCode (Solutions)/LC(189. Rotate Array).java
@@ -1,3 +1,25 @@
-public class LC(189. Rotate Array) {
-    
+class Solution {
+    private void reverse (int[] nums , int left , int right)
+    {
+        while (left <= right)
+        {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+            
+           int n= nums.length;
+           k = k % n;
+           // Standard right rotation
+           // first reverse Entire array
+            //Second reverse first K elements that is k-1 as per index
+            //Third reverse every else  elements that is k to n-1 as per index
+           reverse(nums, 0 , n-1 );
+           reverse(nums, 0  , k-1);
+           reverse(nums , k , n-1);
+    }
 }
