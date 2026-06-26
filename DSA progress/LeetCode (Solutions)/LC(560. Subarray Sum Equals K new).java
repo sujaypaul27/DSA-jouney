3117ded826bceeca4297cc9afc2a22a0cf974a60
Sujay Paul <sujaypaul2711@gmail.com>
2026-06-26 13:11:29 +0530

    Added Brute force code

--- DSA progress/LeetCode (Solutions)/LC(560. Subarray Sum Equals K new).java
+++ DSA progress/LeetCode (Solutions)/LC(560. Subarray Sum Equals K new).java
@@ -23,3 +23,21 @@ class Solution {
 
 
 
+///                       Brute Force 
+//class Solution {     
+  //  public int subarraySum(int[] nums, int k) {
+//  int n = nums.length;
+ //     int count =0;
+  //    for(int i=0 ; i < n ; i++){
+   //     int sum =0;
+    //    for(int j=i;j<n ;j++){
+      //      sum+= arr[j];
+    //    if(sum == k)
+    //        {
+    //            count ++;
+    //        }
+    //    }
+    //  }
+    //  return count ;
+    //}
+//}
