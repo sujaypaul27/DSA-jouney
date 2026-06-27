89bc36152177de32106c103d62df93759a5c98e1
Sujay Paul <sujaypaul2711@gmail.com>
2026-06-27 21:51:08 +0530

    Added solution

--- DSA progress/LeetCode (Solutions)/LC(151. Reverse Words in a String).java
+++ DSA progress/LeetCode (Solutions)/LC(151. Reverse Words in a String).java
@@ -1,1 +1,27 @@
-dsd
+class Solution {
+    public String reverseWords(String s) {
+        int n = s.length();
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while(i < n)
+        {
+            while(i < n && s.charAt(i) == ' ' ) i++;
+            if (i == n)  break;
+            j = i + 1;// important to remove out of index
+            while( j < n  && s.charAt(j) != ' ' ) j++;
+            
+            String value = s.substring( i , j );
+            
+            if(result.length() == 0)
+            {
+                result.append(value);
+            }else
+            {
+                result.insert(0,value + " ");
+            }
+            i = j+1;
+        }
+        return result.toString();
+    }
+}
