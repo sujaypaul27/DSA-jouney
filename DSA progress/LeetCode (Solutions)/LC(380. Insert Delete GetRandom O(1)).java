5f1a52510fa0893f7b5238e269311c9845e1997f
Sujay Paul <sujaypaul2711@gmail.com>
2026-07-01 14:15:20 +0530

    Added Solution

--- DSA progress/LeetCode (Solutions)/LC(380. Insert Delete GetRandom O(1)).java
+++ DSA progress/LeetCode (Solutions)/LC(380. Insert Delete GetRandom O(1)).java
@@ -1,1 +1,48 @@
-ssd
+class RandomizedSet {
+    private HashMap<Integer ,Integer> map;
+    private List<Integer> list;
+    private Random random; 
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        random =new Random ();
+    }
+    
+    public boolean insert(int val) {
+        boolean isExist =map.containsKey(val);
+        if(!isExist)
+        {
+            list.add(val);
+            map.put(val , list.size() -1);
+        }
+        return !isExist;
+    }
+    
+    public boolean remove(int val) {
+        boolean isExists =map.containsKey(val);
+        if(isExists)
+        {
+            int index = map.get(val);
+            int lastElement = list.get(list.size() -1);
+            list.set(index,lastElement);
+            map.put(lastElement , index);
+
+            list.remove(list.size() -1);// to remove the element index from arraylest
+            map.remove(val);// to remove the index of map otherwise will give true all the time
+        }
+        return isExists;
+    }
+    
+    public int getRandom() {
+        int randIndex = random.nextInt(list.size());
+        return list.get(randIndex);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
