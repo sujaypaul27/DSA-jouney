880f9e9fe110cc7e2e2cdb81dc7526c61727dac9
Sujay Paul <sujaypaul2711@gmail.com>
2026-07-01 15:07:10 +0530

    New code

--- DSA progress/LeetCode (Solutions)/LC(128. Longest Consecutive Sequence).java
+++ DSA progress/LeetCode (Solutions)/LC(128. Longest Consecutive Sequence).java
@@ -1,3 +1,27 @@
-public class LC(128. Longest Consecutive Sequence) {
-    
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet <Integer> set = new HashSet<> ();
+        if (nums.length == 0 ) return 0;
+        for (int num : nums)
+        {
+            set.add(num);
+        }
+        int longest = 0;
+        for( int num : set)// for loop use pani normal array ve travel panama yen set la travel panurom na TLE varuthu so reperated elements ku thirupui calculate panuthu . so set la transverse pananum
+        {
+            int currentCount = 0;
+            if(!set.contains(num -1 ))
+            {
+                int number = num;
+                while(set.contains(number ))
+                {
+                    number++;
+                    currentCount++;
+                    
+                }
+                longest = Math.max(longest,currentCount);
+            }
+        }
+        return longest ;
+    }
 }
